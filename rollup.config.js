1b7e554d1dab70a62f5ec315ab8171c718f8f6dc
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-09-11 13:54:14 +0300

    rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,53 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
-import gzipPlugin from 'rollup-plugin-gzip'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+import gzipPlugin from 'rollup-plugin-gzip';
 import buble from '@rollup/plugin-buble';
 import typescript from 'rollup-plugin-typescript2';
 
 export default [
     // browser-friendly UMD build
     {
-        input: "src/genetic.ts",
+        input: 'src/genetic.ts',
         output: {
-            name: "genetic",
+            name: 'genetic',
             file: pkg.browser,
-            format: "umd"
+            format: 'umd',
         },
         plugins: [
             resolve(), // so Rollup can find `ms`
             commonjs(), // so Rollup can convert `ms` to an ES module
-            typescript(),
+            typescript({
+                compilerOptions: {
+                    module: 'ESNext',
+                },
+            }),
             buble({
                 transforms: { forOf: false },
                 objectAssign: 'Object.assign',
-                asyncAwait: false
+                asyncAwait: false,
             }),
             terser(), // uglify
-            gzipPlugin()
-        ]
+            gzipPlugin(),
+        ],
     },
     {
-        input: "src/genetic.ts",
-        external: [
-            ...Object.keys(pkg.dependencies || {}),
-            ...Object.keys(pkg.peerDependencies || {}),
-        ],
+        input: 'src/genetic.ts',
+        external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
         plugins: [
             typescript({
                 tsconfigOverride: {
                     compilerOptions: {
-                        target: "ES2020",
-                    }
-                }
+                        module: 'ESNext',
+                        target: 'ES2020',
+                    },
+                },
             }),
         ],
         output: [
-            { file: pkg.main, format: "cjs" },
-            { file: pkg.module, format: "es" }
-        ]
-    }
+            { file: pkg.main, format: 'cjs' },
+            { file: pkg.module, format: 'es' },
+        ],
+    },
 ];
